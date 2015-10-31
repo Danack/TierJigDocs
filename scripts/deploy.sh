3f483efe4ef1feaf40f0c954294e4fcf749fc6d0
Danack <Danack@basereality.com>
2015-10-31 00:26:56 +0000

    Lets read the environment param why not.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,11 @@ set -eux -o pipefail
 
 environment="centos_guest,dev"
 
+if [ "$#" -ge 1 ]; then
+    environment=$1
+fi
+
+
 find . -name "*.sh" -exec chmod 755 {} \;
 
 su tierjigdocs -c "./scripts/deployAsUser.sh ${environment}"
